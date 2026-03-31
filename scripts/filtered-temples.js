63dc51bfc6b121591be51004f152174bdb432612
ediCarlosIhs <ecarlosdasilva@byupathway.edu>
2026-03-31 11:02:24 -0300

    w04 project done.

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,5 +1,12 @@
 const hamButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
+const templesList = document.querySelector("#templesList");
+
+const homeLink = document.querySelector("#allTemples");
+const oldLink = document.querySelector("#oldTemples");
+const newLink = document.querySelector("#newTemples");
+const largeLink = document.querySelector("#largeTemples");
+const smallLink = document.querySelector("#smallTemples");
 
 const temples = [
   {
@@ -8,7 +15,7 @@ const temples = [
     dedicated: "2005, August, 7",
     area: 11500,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    "images/temples/temple-01.jpg"
   },
   {
     templeName: "Manti Utah",
@@ -16,7 +23,7 @@ const temples = [
     dedicated: "1888, May, 21",
     area: 74792,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    "images/temples/temple-02.jpg"
   },
   {
     templeName: "Payson Utah",
@@ -24,7 +31,7 @@ const temples = [
     dedicated: "2015, June, 7",
     area: 96630,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    "images/temples/temple-03.jpg"
   },
   {
     templeName: "Yigo Guam",
@@ -32,7 +39,7 @@ const temples = [
     dedicated: "2020, May, 2",
     area: 6861,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    "images/temples/temple-04.jpg"
   },
   {
     templeName: "Washington D.C.",
@@ -40,7 +47,7 @@ const temples = [
     dedicated: "1974, November, 19",
     area: 156558,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    "images/temples/temple-05.jpg"
   },
   {
     templeName: "Lima Perú",
@@ -48,18 +55,18 @@ const temples = [
     dedicated: "1986, January, 10",
     area: 9600,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    "images/temples/temple-06.jpg"
   },
   {
-    templeName: "Mexico City Mexico",
+    templeName: "Mexico City",
     location: "Mexico City, Mexico",
     dedicated: "1983, December, 2",
     area: 116642,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    "images/temples/temple-07.jpg"
   },
   {
-    templeName: "Belém Brazil Temple",
+    templeName: "Belém Brazil",
     location: "Belém-PA, Brazil",
     dedicated: "2022, November, 20",
     area: 28675,
@@ -67,7 +74,7 @@ const temples = [
     "images/temples/temple-08.webp"
   },
   {
-    templeName: "Brasília Brazil Temple",
+    templeName: "Brasília Brazil",
     location: "Brasilia-DF, Brazil",
     dedicated: "2020, September, 26",
     area: 25000,
@@ -75,7 +82,7 @@ const temples = [
     "images/temples/temple-09.webp"
   },
   {
-    templeName: "Campinas Brazil Temple",
+    templeName: "Campinas Brazil",
     location: "Campinas-SP, Brazil",
     dedicated: "1998, May, 1",
     area: 48100,
@@ -89,4 +96,126 @@ hamButton.addEventListener("click", () => {
 
     navigation.classList.toggle("open");
     hamButton.classList.toggle("open");
+});
+
+function createCard(filteredTemples) {
+  templesList.innerHTML = "";
+
+  filteredTemples.forEach(temple => {
+
+
+  
+    const card = document.createElement("div");
+    card.classList.add("temple-card");
+  
+    const h2 = document.createElement("h2");
+    h2.textContent = temple.templeName;
+  
+    const templeInformations = document.createElement("div");
+    templeInformations.classList.add("temple-informations");
+
+    let location = temple.location.split(",");
+    location = `${location.at(0)}, ${location.at(-1)}`;
+
+    let dedication = temple.dedicated.split(",");
+    dedication = `${dedication.at(1)}, ${dedication.at(0)}`
+
+    templeInformations.innerHTML = `<ul>
+      <li>
+          <span class="data-label">Location: </span>
+          <span class="data-information">${location}</span>
+      </li>
+      <li>
+          <span class="data-label">Dedicated: </span>
+          <span class="data-information">${dedication}</span>
+      </li>
+      <li>
+          <span class="data-label">Size: </span>
+          <span class="data-information">${temple.area} sq fit</span>
+      </li>
+    </ul>`
+  
+    const picture = document.createElement("picture");
+    picture.innerHTML = `<img src=${temple.imageUrl} alt=An image of ${temple.templeName} loading="lazy" width="400" height="250">`
+  
+    card.appendChild(h2);
+  
+    card.appendChild(templeInformations);
+  
+    card.appendChild(picture);
+  
+    templesList.appendChild(card);
+  });
+
+}
+
+createCard(temples);
+
+function removeActiveClass() {
+  const activeElement = document.querySelector(".active");
+  if (activeElement) {
+    activeElement.classList.remove("active");
+  }
+}
+
+homeLink.addEventListener("click", (event) => {
+
+  removeActiveClass()
+  event.target.classList.add("active");
+
+  createCard(temples);
+});
+
+oldLink.addEventListener("click", (event) => {
+
+  removeActiveClass()
+  event.target.classList.add("active");
+
+  const oldTemples = temples.filter(temple => {
+    let templeDedication = parseInt(temple.dedicated.split(",")[0]);
+    return templeDedication < 1900;
+  });
+  // debugger;
+  createCard(oldTemples);
+});
+
+
+
+newLink.addEventListener("click", (event) => {
+
+  removeActiveClass()
+  event.target.classList.add("active");
+
+  const newTemples = temples.filter(temple => {
+    let templeDedication = parseInt(temple.dedicated.split(",")[0]);
+    return templeDedication > 2000;
+  });
+  // debugger;
+  createCard(newTemples);
+});
+
+largeLink.addEventListener("click", (event) => {
+
+  removeActiveClass()
+  event.target.classList.add("active");
+
+  const largeTemples = temples.filter(temple => {
+    let area = temple.area;
+    return area > 90000;
+  });
+  // debugger;
+  createCard(largeTemples);
+});
+
+smallLink.addEventListener("click", (event) => {
+
+  removeActiveClass()
+  event.target.classList.add("active");
+
+  const smallTemples = temples.filter(temple => {
+    let area = temple.area;
+    return area < 10000;
+  });
+  // debugger;
+  createCard(smallTemples);
 });
